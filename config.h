8104706638505f79125ddde55c4b4537bb530155
nemolize <nemolize@users.noreply.github.com>
2020-08-03 20:08:22 +0900

    chore: tweak framerate

--- config.h
+++ config.h
@@ -56,7 +56,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
 
-#define MOUSEKEY_FRAMERATE 100
+#define MOUSEKEY_FRAMERATE 144
 #define MOUSEKEY_CURSOR_FORCE 412
 #define MOUSEKEY_CURSOR_MASS 1
 #define MOUSEKEY_CURSOR_FRICTION 33
